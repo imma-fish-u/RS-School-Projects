8a6004883f434378add5763ee1c95db9a0c7dd09
imma-fish-u <stankoagregatBel@gmail.com>
2021-09-27 20:34:06 +0300

    feat: add project

---
+++ museum/js/script.js
@@ -0,0 +1,137 @@
+const progress = document.querySelectorAll('.progress-range');
+
+for (var progressItem of progress) {
+  progressItem.addEventListener('input', function() {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, var(--dark-red) 0%, var(--dark-red) ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
+  })
+}
+
+/*==================== MENU SHOW AND HIDDEN ====================*/
+const mainMenu = document.querySelector('.main-menu'),
+      navToggle = document.getElementById('nav-toggle')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    mainMenu.classList.add('show-menu') 
+  })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists 
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}*/
+
+/*==================== MODAL ====================*/
+
+const modalView = document.querySelector('.tickets-modal__container'),
+      modalBtn = document.querySelector('.tickets-section__button'),
+      modalClose = document.querySelector('.tickets-modal__close'),
+      modalOverlay = document.querySelector('.overlay')
+
+
+modalBtn.addEventListener('click', () => {
+  modalView.classList.add('active-modal')
+  modalOverlay.classList.add('active-overlay')
+})
+
+
+modalClose.addEventListener('click', () => {
+    modalView.classList.remove('active-modal')
+    modalOverlay.classList.remove('active-overlay')
+})
+
+
+modalOverlay.addEventListener(('click'), () => {
+  modalView.classList.remove('active-modal')
+  modalOverlay.classList.remove('active-overlay')
+})
+
+/*==================== EXPLORE SLIDER ====================*/
+
+function initComparisons() {
+  var x, i;
+  x = document.getElementsByClassName("img-comp-overlay");
+  var slider = document.querySelector(".img-comp-slider");
+
+  for (i = 0; i < x.length; i++) {
+    compareImages(x[i]);
+  }
+
+  function compareImages(img) {
+    var img, clicked = 0, w, h;
+    w = img.offsetWidth;
+    h = img.offsetHeight;
+
+    img.style.width = (w / 2) + 50  + "px";
+
+    slider.style.left = (w / 2) - (slider.offsetWidth / 2) + 50 + "px";
+
+    slider.addEventListener("mousedown", slideReady);
+    window.addEventListener("mouseup", slideFinish);
+    slider.addEventListener("touchstart", slideReady);
+    window.addEventListener("touchend", slideFinish);
+
+    function slideReady(e) {
+      e.preventDefault();
+      /* The slider is now clicked and ready to move: */
+      clicked = 1;
+      window.addEventListener("mousemove", slideMove);
+      window.addEventListener("touchmove", slideMove);
+    }
+    function slideFinish() {
+      /* The slider is no longer clicked: */
+      clicked = 0;
+    }
+    function slideMove(e) {
+      var pos;
+      if (clicked == 0) return false;
+
+      pos = getCursorPos(e)
+      if (pos < 0) pos = 0;
+      if (pos > w) pos = w;
+      slide(pos);
+    }
+    function getCursorPos(e) {
+      var a, x = 0;
+      e = (e.changedTouches) ? e.changedTouches[0] : e;
+      /* Get the x positions of the image: */
+      a = img.getBoundingClientRect();
+      /* Calculate the cursor's x coordinate, relative to the image: */
+      x = e.pageX - a.left;
+      /* Consider any page scrolling: */
+      x = x - window.pageXOffset;
+      return x;
+    }
+    function slide(x) {
+      img.style.width = x + "px";
+      slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+    }
+  }
+}
+
+initComparisons();
+
+/*==================== SWIPER ====================*/
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 41,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+ 
---
+++ museum/js/slider.js
@@ -0,0 +1,73 @@
+var slide = 0,
+    welcomeSlider = document.querySelector('.welcome-section__slider'),
+    welcomeSliderBullets = document.querySelector('.welcome-section__slider-bullets'),
+    slides = [],
+    numSlides = 5,
+
+    //Functions!!
+    loadSlides = () => {
+      for (let el = numSlides; el > 0; el--) {
+        let img = document.createElement('div');
+        let bullet = document.createElement('div');
+        img.classList.add('welcome-section__img');
+        bullet.classList.add('welcome-section__slider-bullet');
+        img.style.background = `var(--dark-grad), url(assets/img/welcome-slider/${el}.jpg) no-repeat`;
+        welcomeSlider.prepend(img);
+        slides.push(img);
+
+        if (el == numSlides) {
+          bullet.classList.add('welcome-section__slide-active');
+        }
+        welcomeSliderBullets.append(bullet);
+      }
+    }
+    currentSlide = function() {
+      const slideWidth = slides.clientWidth
+      const offset = -slide * slideWidth
+      
+      welcomeSlider.style.transform = `translate(${offset}px)`;
+    },
+    next = function() {
+      slide++;
+      if (slide >= numSlides) slide = 0;
+      currentSlide();
+    },
+    prev = function() {
+      slide--;
+      if (slide < 0) slide = numSlides - 1;
+      currentSlide();
+    };
+
+
+//Buttons
+loadSlides();
+/*document.querySelector('#first').addEventListener('click', function() {
+    slide = 0;
+    currentSlide();
+}, false);
+document.querySelector('#second').addEventListener('click', function() {
+    slide = 1;
+    currentSlide();
+}, false);
+document.querySelector('#third').addEventListener('click', function() {
+    slide = 2;
+    currentSlide();
+}, false);
+document.querySelector('#fourth').addEventListener('click', function() {
+    slide = 3;
+    currentSlide();
+}, false);
+document.querySelector('#fifth').addEventListener('click', function() {
+    slide = 4;
+    currentSlide();
+}, false);
+document.querySelector('#sixth').addEventListener('click', function() {
+    slide = 5;
+    currentSlide();
+}, false);*/
+document.querySelector('.welcome-section__right-arrow').addEventListener('click', function() {
+    next();
+}, false);
+document.querySelector('.welcome-section__left-arrow').addEventListener('click', function() {
+    prev();
+}, false);
